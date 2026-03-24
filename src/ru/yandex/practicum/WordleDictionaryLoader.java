870f8f11618d2811bbe300e63909c8b91fb83130
CotU <CotUniverse@yandex.ru>
2026-03-24 17:50:14 +0700

    Добавить чтение слов из словаря

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,33 @@
 package ru.yandex.practicum;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+    private final Charset standardCharsets = StandardCharsets.UTF_8;
+
+    public WordleDictionary openDictionary()  {
+        List <String> wordleDictionary = new ArrayList<>();
+        String fileName = "words_ru.txt";
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName), standardCharsets)) {
+            while (br.ready()) {
+               wordleDictionary.add(br.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении." + fileName);
+        }
+
+        return new WordleDictionary(wordleDictionary);
+    }
+
 }
