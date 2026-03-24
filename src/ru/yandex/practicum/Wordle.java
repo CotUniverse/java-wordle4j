a47614ed599b2675751c14e4eeebbc53145b602f
CotU <CotUniverse@yandex.ru>
2026-03-24 21:08:05 +0700

    Добавить цикл для игры и лог файл

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 /*
 в главном классе нам нужно:
     создать лог-файл (он должен передаваться во все классы)
@@ -12,6 +15,35 @@ package ru.yandex.practicum;
 public class Wordle {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = "words_ru.txt";
+
+        try (PrintWriter logWriter = new PrintWriter(new FileWriter("game.log", true));){
+
+            WordleDictionaryLoader loader = new WordleDictionaryLoader();
+
+            WordleDictionary myDictionary = loader.openDictionary(fileName);
+
+            WordleGame wordleGame = new WordleGame(myDictionary, logWriter);
+
+            wordleGame.getRandomWord();
+
+            while (wordleGame.hasSteps()) {
+                System.out.println("Введите слово");
+                String input = scanner.nextLine();
+
+                String hint = wordleGame.checkWord(input);
+                if (hint.equals("+++++")) {
+                    System.out.println("Вы победили!");
+                    break;
+                }
+                else {
+                    System.out.println(hint);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
