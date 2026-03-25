31511af96fedc7c8a7306fe92e01afe4fd5cc964
CotU <CotUniverse@yandex.ru>
2026-03-25 12:09:00 +0700

    Добавить тесты для openDictionary в WordleDictionaryLoader

--- test/ru/yandex/practicum/WordleTest.java
+++ test/ru/yandex/practicum/WordleTest.java
@@ -1,7 +1,27 @@
 package ru.yandex.practicum;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordleTest {
 
+    @Test
+    void testOpenDictionary() throws IOException {
+        try (PrintWriter logWriter = new PrintWriter(new FileWriter("test_game.log", true))) {
+            WordleDictionaryLoader loader = new WordleDictionaryLoader(logWriter);
+
+            Assertions.assertNotNull(loader.openDictionary("words_ru.txt"));
+            Assertions.assertTrue(loader.openDictionary("words_en.txt").getWords().isEmpty(),
+                    "Ожидается пустой словарь для несуществующего файла");
+        } catch (IOException e) {
+            System.out.println("Прозошла ошибка");;
+        }
+    }
 }
